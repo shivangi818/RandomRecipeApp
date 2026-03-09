20ffedde0325c4df529c402999cfb7c93608513a
shivangi818 <shivangisahay2004@gmail.com>
2026-03-09 21:06:05 +0530

    connect deployed backend

--- Frontend/App.js
+++ Frontend/App.js
@@ -1,5 +1,4 @@
-const API_URL = "http://127.0.0.1:5000/recipes";
-
+const API_URL = "https://randomrecipeapp.onrender.com/recipes";
 // Elements
 const randomBtn = document.getElementById("randomBtn");
 const randomRecipeDiv = document.getElementById("randomRecipe");
